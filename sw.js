64ef802905ab4281223bb9644171fc3fe669d26f
PicWei <weiqiyuan5560@dingtalk.com>
2020-03-12 17:20:00 +0800

    Second Push

--- sw.js
+++ sw.js
@@ -1,42 +1,28 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-1'
-var cacheList=[
-  '/',
-  'index.html',
+var cacheName = 'my-cache'
+var cacheList = ['index.html',
   'main.css',
-  'youhun.jpg'
-]
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  )
-})
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
+  'youhun.jpg']
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    // 安装成功后向caches中存入需要缓存的文件  
+    caches.open(cacheName).then(function (cache) {
+        return cache.addAll(cacheList)
     })
   )
-})
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
+});
+// 监听service worker fetch
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+        .then(function(response) {
+            // 在缓存中查找到匹配的请求，就从缓存返回
+            if (response) {
+                console.log(response)
+                return response;
+            }
+            // 缓存中没有查找到对应请求，继续网络请求
+            return fetch(event.request);
+        }
+    )
+  );
 })
