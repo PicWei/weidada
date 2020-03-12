f020f85b02c5ddd801e1823b17d65069eac2dda0
PicWei <weiqiyuan5560@dingtalk.com>
2020-03-12 17:41:25 +0800

    Second Push

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-var cacheName = 'my-cache';
-var cacheList = ['index.html',
-  'main.css',
-  'youhun.jpg']
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  );
+var cacheName = 'hello-pwa';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+          .then(cache => cache.addAll(
+            [
+                '/',  // 这个一定要包含整个目录，不然无法离线浏览
+                'index.html',
+				'main.css',
+				'youhun.jpg'
+            ]
+          )).then(() => self.skipWaiting())
+    );
 });
-// 监听service worker fetch
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-        .then(function(response) {
-            // 在缓存中查找到匹配的请求，就从缓存返回
-            if (response) {
-                console.log(response)
-                return response;
-            }
-            // 缓存中没有查找到对应请求，继续网络请求
-            return fetch(event.request);
+
+self.addEventListener('fetch', function (event) {  
+    event.respondWith(
+      caches.match(event.request)                    
+      .then(function (response) {
+        if (response) {                              
+          return response;                           
         }
-    )
-  );
+        return fetch(event.request);                 
+      })
+    );
 });
