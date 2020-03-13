3a1cfd47bed8c789dad37cb59899d0b509ded1d0
PicWei <weiqiyuan5560@dingtalk.com>
2020-03-13 10:45:36 +0800

    second day

--- sw.js
+++ sw.js
@@ -1,43 +1,44 @@
-var cacheName = 'helloWorld';     // 缓存的名称  
-// install 事件，它发生在浏览器安装并注册 Service Worker 时        
-self.addEventListener('install', event => { 
-/* event.waitUtil 用于在安装成功之前执行一些预装逻辑
- 但是建议只做一些轻量级和非常重要资源的缓存，减少安装失败的概率
- 安装成功后 ServiceWorker 状态会从 installing 变为 installed */
-  event.waitUntil(
-    caches.open(cacheName)                  
-    .then(cache => cache.addAll([    // 如果所有的文件都成功缓存了，便会安装完成。如果任何文件下载失败了，那么安装过程也会随之失败。        
-      '/index.html',
-      '/main.css',
-	  '/youhun.jpg'
-    ]))
-  );
-});
-  
-/**
-为 fetch 事件添加一个事件监听器。接下来，使用 caches.match() 函数来检查传入的请求 URL 是否匹配当前缓存中存在的任何内容。如果存在的话，返回缓存的资源。
-如果资源并不存在于缓存当中，通过网络来获取资源，并将获取到的资源添加到缓存中。
-*/
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)                  
-    .then(function (response) {
-      if (response) {                            
-        return response;                         
-      }
-    })
-      var requestToCache = event.request.clone();  //          
-      return fetch(requestToCache).then(                   
-        function (response) {
-          if (!response || response.status !== 200) {      
-            return response;
+var cacheName = 'gotop-06'
+
+var cacheList = [
+  "/",
+  "index.html",
+  "dist/app.js",
+]
+
+// Service Worker 注册完成事件，写入缓存
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(cacheList))
+      .then(() => self.skipWaiting())
+  )
+})
+
+// Service Worker 启动事件，处理更新缓存
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(keyList => {
+        return keyList.map(key => {
+          // 遍历已有缓存，保留与当前一样的缓存，即更新
+          if (key !== cacheName) {
+            return caches.delete(key)
           }
-          var responseToCache = response.clone();          
-          caches.open(cacheName)                           
-            .then(function (cache) {
-              cache.put(requestToCache, responseToCache);  
-            });
-          return response;             
-    });
- 
-)});
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
+
+// 请求接口事件，处理相关逻辑
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    // 如果缓存内容存在，则提供缓存内容，否则继续 fetch
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request.url)
+    })
+  )
+})
